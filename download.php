2c6176d5e073fffdff41e97fda64c2a3b43d728a
ichirugame <101532725+ichirugame@users.noreply.github.com>
2023-09-02 20:08:16 +0900

    初期

---
+++ download.php
@@ -0,0 +1,56 @@
+<?php
+if(isset($_GET['key'], $_GET['file_name'])){
+    if(preg_match("/[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[一-龠]+|[ａ-ｚＡ-Ｚ０-９]/u", $_GET['file_name'])){
+        $file_name = __DIR__.'/upload/'.$_GET['key'].'/'.$_GET['file_name'];
+        $file_name = mb_convert_encoding($file_name, 'sjis', 'auto');
+    }else{
+        $file_name = __DIR__.'/upload/'.$_GET['key'].'/'.$_GET['file_name'];
+    }
+}
+if(isset($_GET['key']) and empty($_GET['file_name']) and file_exists(__DIR__.'/upload/'.$_GET['key'])){
+    $dir = __DIR__.'/upload/'.$_GET['key'];
+    $dir_array = glob($dir.'/*');
+}elseif(isset($_GET['key'], $_GET['file_name']) and is_file($file_name)){
+    $file = __DIR__.'/upload/'.$_GET['key'].'/'.$_GET['file_name'];
+    $filetype = pathinfo($file, PATHINFO_EXTENSION);
+    header("Content-type: application/$filetype");
+    header('Content-Disposition: attachment; filename="'.$_GET['file_name'].'"');
+    header('Content-Length: '.filesize($file));
+    readfile($file);
+    exit;
+}else{
+    $error = 'エラーが発生しました.';
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ダウンロード</title>
+</head>
+<body>
+    <?php
+    if(empty($_GET['key'])){
+    ?>
+    <p>共有されたコードを記入して送信してください</p>
+    <form action="" method="get">
+        <input type="text" name="key">
+        <input type="submit">
+    </form>
+    <?php
+    }elseif(isset($_GET['key'], $dir_array)){
+    ?>
+    <p>ファイル一覧</p>
+    <?php
+    foreach($dir_array as $file){
+        echo '<p>ファイル名: <a href="./download.php?key='.$_GET['key'].'&file_name='.basename($file).'"</a>'.basename($file).'</p>';
+    }
+    ?>
+    <?php
+    }elseif(isset($error)){
+        echo $error;
+    }
+    ?>
+</body>
+</html>
---
+++ upload.php
@@ -0,0 +1,35 @@
+<?php
+if(isset($_FILES["files"])){
+    $dir = md5(uniqid(rand(), true));
+    mkdir(__DIR__.'/upload/'.$dir, 0700);
+    if(!file_exists(__DIR__.'/upload/'.$dir)){
+        exit;
+    }
+    for($i = 0; $i < count($_FILES["files"]["name"]); $i++ ){
+        if(is_uploaded_file($_FILES["files"]["tmp_name"][$i])){
+            move_uploaded_file($_FILES["files"]["tmp_name"][$i],__DIR__."/upload/".$dir.'/'.basename($_FILES["files"]["name"][$i]));
+        }
+    }
+    $comment = "アップロード完了しました。\n以下のコードを共有することによって誰でもダウンロードすることができます。\n$dir";
+}
+?>
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>アップロード</title>
+</head>
+<body>
+    <?php
+    if(isset($comment)){
+        echo nl2br($comment);
+    }
+    ?>
+    <p>ファイルをアップロードしてください</p>
+    <form action="" method="post" enctype="multipart/form-data">
+        <input type="file" name="files[]" multiple>
+        <input type="submit">
+    </form>
+</body>
+</html>
